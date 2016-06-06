5f0ec4b85f9cde6e7108800b24cc4b7c99c0ed7f
ikcam <me@ikcam.com>
2016-06-06 17:59:32 -0500

    Change extensions

--- docs/conf.py
+++ docs/conf.py
@@ -33,9 +33,12 @@ import boilerplate
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = [
-    'djangodocs',
     'sphinx.ext.autodoc',
+    'sphinx.ext.doctest',
     'sphinx.ext.intersphinx',
+    'sphinx.ext.todo',
+    'sphinx.ext.coverage',
+    'sphinx.ext.mathjax',
     'sphinx.ext.viewcode',
 ]
 
